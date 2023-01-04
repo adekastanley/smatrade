5e0ddd63592fd25a3688378be57709c34605b773
adekastanley <adekastanley2@gmail.com>
2023-01-04 20:44:36 +0100

    test upload, resourse sorting added, needs work

--- createaccount/acoount.js
+++ createaccount/acoount.js
@@ -30,3 +30,85 @@ function changeStep(btn){
     }
     steps[index].classList.add('active')
 }
+// **********************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const filterResources = document.querySelector( '.filter-resources');
+const allResources = Array.from(document.querySelectorAll('.resource'));
+const filterResources = document.querySelector( '#filter-resources')
+const filterForm = document.querySelector( '#filter-form');
+
+
+
+filterForm.addEventListener('click', () => {
+    if (filterResources.value === 'all'){
+        allResources.forEach( e => {
+            e.classList.add('show-resource')
+            e.classList.remove('hide-resource')
+        })
+    }
+    if (filterResources.value === 'E-dividend'){
+        allResources.forEach( e => {
+            if (e.classList.contains( "e-dividend")){
+
+                e.classList.add('show-resource')
+            } else {
+                e.classList.add('hide-resource')
+                
+
+                
+            }
+        })
+    }
+    if (filterResources.value === 'fgn'){
+        allResources.forEach( e => {
+            if (e.classList.contains( "fgn")){
+                e.classList.remove('hide-resource')
+                e.classList.add('show-resource')
+            } else {
+                e.classList.remove('show-resource')
+                e.classList.add('hide-resource')
+            }
+        })
+    }
+    if (filterResources.value === 'transfer'){
+        allResources.forEach( e => {
+            if (e.classList.contains( "transfer")){
+                e.classList.remove('hide-resource')
+                e.classList.add('show-resource')
+            } else {
+                e.classList.remove('show-resource')
+                e.classList.add('hide-resource')
+            }
+        })
+    }
+    if (filterResources.value === 'update'){
+        allResources.forEach( e => {
+            if (e.classList.contains( "update")){
+                e.classList.remove('hide-resource')
+                e.classList.add('show-resource')
+            } else {
+                e.classList.remove('show-resource')
+                e.classList.add('hide-resource')
+            }
+        })
+    }
+
+})
+
+
+// ediv.addEventListener('click', ()=> {
+//     alert(1)
+// })
+// console.log(filterResources.outerText)
