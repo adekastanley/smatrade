b8e122cdd76a5662d23ba90671a8be75ce57876b
adekastanley <adekastanley2@gmail.com>
2023-01-05 22:23:24 +0100

    working on live data, removed node, using fetch now

--- stockData.js
+++ stockData.js
@@ -1,28 +1,67 @@
-const request = require('request');
-let key = '9039772849a1495c9b8562f22d9f459a'
+// https://api.twelvedata.com/price?symbol=AAPL&apikey=demo
+let ld = document.querySelector('.live-data')
+let metaPrice = document.querySelector('.meta-price')
+let googlePrice = document.querySelector('.google-price')
+let microsoftPrice = document.querySelector('.microsoft-price')
+let adidasPrice = document.querySelector('.adidas-price')
+let nikePrice = document.querySelector('.nike-price')
+let teslaPrice = document.querySelector('.tesla-price')
 
+// function refreshPrices (){
+        
+const getStockPrice = async (stock) => {
 
-async function CurrentPrices(ticks ){
-
-
-
-
-let tdurl = 'https://api.twelvedata.com/price?symbol='+ ticks.toString() +'&apikey='+ key;
-
-request.get({
-    url: tdurl,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err, res, data) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      // data is successfully parsed as a JSON object:
-      console.log(data);
-    }
-});
+    const response = await fetch( ' https://api.twelvedata.com/price?symbol='+ stock +'&apikey=9039772849a1495c9b8562f22d9f459a')
+    const data = await response.json()
+    return data
 }
 
-CurrentPrices(["IBM","META"])
+getStockPrice('META').then(
+    data => {
+
+        let price = data.price
+        metaPrice.textContent = price
+      
+    }
+)
+getStockPrice('GOOG').then(
+    data => {
+
+        let price = data.price
+        googlePrice.textContent = price
+
+      
+    }
+)
+getStockPrice('MSFT').then(
+    data => {
+        let price = data.price
+        microsoftPrice.textContent = price
+
+      
+    }
+)
+getStockPrice('ADS').then(
+    data => {
+
+        let price = data.price
+        adidasPrice.textContent = price
+ 
+    }
+)
+getStockPrice('NKE').then(
+    data => {
+
+        let price = data.price
+        nikePrice.textContent = price   
+    }
+)
+getStockPrice('TSLA').then(
+    data => {
+
+        let price = data.price
+        teslaPrice.textContent = price      
+    }
+)
+// }
+// setInterval( refreshPrices, 60000)
