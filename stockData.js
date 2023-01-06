2457395f3e98e55d25f8e26a9132cf1b6c3b005e
adekastanley <adekastanley2@gmail.com>
2023-01-06 16:23:50 +0100

    live data change, makes a single api call now

--- stockData.js
+++ stockData.js
@@ -14,54 +14,18 @@ const getStockPrice = async (stock) => {
     const response = await fetch( ' https://api.twelvedata.com/price?symbol='+ stock +'&apikey=9039772849a1495c9b8562f22d9f459a')
     const data = await response.json()
     return data
-}
+} 
 
-getStockPrice('META').then(
+getStockPrice(['META', 'GOOG','MSFT','NKE','TSLA','ADS']).then(
     data => {
 
-        let price = data.price
-        metaPrice.textContent = price
-      
+        // console.log(data['META'].price )
+        metaPrice.textContent =  '$' + data['META'].price 
+        googlePrice.textContent =  '$' + data['GOOG'].price 
+        microsoftPrice.textContent = '$' + data['MSFT'].price 
+        adidasPrice.textContent =  '$' + data['ADS'].price 
+        nikePrice.textContent =  '$' + data['NKE'].price 
+        teslaPrice.textContent =  '$' + data['TSLA'].price 
+
     }
 )
-getStockPrice('GOOG').then(
-    data => {
-
-        let price = data.price
-        googlePrice.textContent = price
-
-      
-    }
-)
-getStockPrice('MSFT').then(
-    data => {
-        let price = data.price
-        microsoftPrice.textContent = price
-
-      
-    }
-)
-getStockPrice('ADS').then(
-    data => {
-
-        let price = data.price
-        adidasPrice.textContent = price
- 
-    }
-)
-getStockPrice('NKE').then(
-    data => {
-
-        let price = data.price
-        nikePrice.textContent = price   
-    }
-)
-getStockPrice('TSLA').then(
-    data => {
-
-        let price = data.price
-        teslaPrice.textContent = price      
-    }
-)
-// }
-// setInterval( refreshPrices, 60000)
